2e4a3dc250dc7d48d69c30fd732dcef33dcba0e0
skw1105 <skw1105@gmail.com>
2022-09-04 04:14:08 +0900

    예외처리 공통 에러 핸들러 생성 및 적용

--- controller/global-stat.controller.js
+++ controller/global-stat.controller.js
@@ -1,4 +1,5 @@
 const {GlobalStat} = require('../database');
+const {wrapWithErrorHandler} = require('../util');
 
 // getAll()
 // insert
@@ -15,25 +16,40 @@ async function getAll(req, res) {
 
 // 추가/수정
 async function insertOrUpdate(req, res) {
-    try {
-        const {cc, date} = req.body;
-        if (!cc || !date) {
-            res.status(400)
-            .json({ error: 'cc and date are required' });
-            return;
-        }
-        const count = await GlobalStat.count({ where: {cc, date} });
-        if (count === 0) {
-            await GlobalStat.create(req.body);
-        } else {
-            await GlobalStat.update(req.body, { where: {cc, date} });
-        }
+    // try {
+    //     const {cc, date} = req.body;
+    //     if (!cc || !date) {
+    //         res.status(400)
+    //         .json({ error: 'cc and date are required' });
+    //         return;
+    //     }
+    //     const count = await GlobalStat.count({ where: {cc, date} });
+    //     if (count === 0) {
+    //         await GlobalStat.create(req.body);
+    //     } else {
+    //         await GlobalStat.update(req.body, { where: {cc, date} });
+    //     }
 
-        res.status(200)
-            .json({ result: 'success' });
-    } catch (ex) {
-        res.status(500).json({ error: ex.toString() });
+    //     res.status(200)
+    //         .json({ result: 'success' });
+    // } catch (ex) {
+    //     res.status(500).json({ error: ex.toString() });
+    // }
+    const {cc, date} = req.body;
+    if (!cc || !date) {
+        res.status(400)
+        .json({ error: 'cc and date are required' });
+        return;
     }
+    const count = await GlobalStat.count({ where: {cc, date} });
+    if (count === 0) {
+        await GlobalStat.create(req.body);
+    } else {
+        await GlobalStat.update(req.body, { where: {cc, date} });
+    }
+
+    res.status(200)
+        .json({ result: 'success' });
 }
 
 // 삭제
@@ -56,10 +72,14 @@ async function remove(req, res) {
 
 
 //
-module.exports = {
+// module.exports = {
+//     getAll,
+//     insertOrUpdate,
+//     remove
+// };
+module.exports = wrapWithErrorHandler({
     getAll,
     insertOrUpdate,
     remove
-};
-
+});
 
